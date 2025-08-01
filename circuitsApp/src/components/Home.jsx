cb5b8aa07598902d6b9a8cb2826dd1bc8633a620
Roberto Velez Iniguez <robertojvi@gmail.com>
2025-07-31 20:44:04 -0500

    Refactor Home component layout by removing navigation and adjusting styles for improved user experience

--- circuitsApp/src/components/Home.jsx
+++ circuitsApp/src/components/Home.jsx
@@ -1,63 +1,9 @@
 function Home() {
 	return (
-		<div style={{ paddingTop: "50px", display: "flex", width: "100%" }}>
-			<nav
-				style={{
-					width: "250px",
-					minHeight: "calc(100vh - 50px)",
-					backgroundColor: "#2c3e50",
-					padding: "20px",
-					position: "fixed",
-					left: 0,
-					top: "50px",
-					zIndex: 999,
-				}}
-			>
-				<ul
-					style={{
-						listStyle: "none",
-						padding: 0,
-						color: "#ecf0f1",
-						fontSize: "16px",
-					}}
-				>
-					<li
-						style={{
-							marginBottom: "15px",
-							padding: "10px",
-							cursor: "pointer",
-							"&:hover": { backgroundColor: "#34495e" },
-						}}
-					>
-						Dashboard
-					</li>
-					<li
-						style={{
-							marginBottom: "15px",
-							padding: "10px",
-							cursor: "pointer",
-							"&:hover": { backgroundColor: "#34495e" },
-						}}
-					>
-						Overview
-					</li>
-					<li
-						style={{
-							marginBottom: "15px",
-							padding: "10px",
-							cursor: "pointer",
-							"&:hover": { backgroundColor: "#34495e" },
-						}}
-					>
-						Quick Links
-					</li>
-				</ul>
-			</nav>
+		<div style={{ paddingTop: "50px", width: "100%" }}>
 			<div
 				style={{
-					marginLeft: "250px",
 					padding: "20px",
-					flex: 1,
 					textAlign: "center",
 				}}
 			>
@@ -65,12 +11,12 @@ function Home() {
 					src="src/images/Access.png"
 					alt="Access Parks Logo"
 					style={{
-						width: "200px",
+						width: "150px",
 						marginBottom: "20px",
 					}}
 				/>
 				<h1>AccessParks Circuits</h1>
-				<p>Select an Option from the top Menu</p>
+				<h3>Select an Option from the top Menu</h3>
 			</div>
 		</div>
 	);
