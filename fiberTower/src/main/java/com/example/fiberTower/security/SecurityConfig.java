817e452757179fe2ec673679d4a8a1b717b0edb9
Roberto Velez Iniguez <robertojvi@gmail.com>
2026-03-26 14:21:35 -0500

    feat: update CORS configuration to use a list for allowed origin patterns

--- fiberTower/src/main/java/com/example/fiberTower/security/SecurityConfig.java
+++ fiberTower/src/main/java/com/example/fiberTower/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns("*");
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
